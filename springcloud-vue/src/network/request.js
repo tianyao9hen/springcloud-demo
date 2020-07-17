88dc7522040c2a80923468529a2d07d7650eb6e3
pxf <398862770@qq.com>
2020-07-17 15:53:01 +0800

    解决请求头的问题

--- springcloud-vue/src/network/request.js
+++ springcloud-vue/src/network/request.js
@@ -4,10 +4,16 @@
 import axios from 'axios'
 
 export function request(config) {
+    let tokenHeader = {}
+    if(sessionStorage.getItem("authorization")){
+        tokenHeader = {
+            'token': sessionStorage.getItem('authorization'),
+        }
+    }
     const instance = axios.create({
         baseURL: "http://localhost",
-        timeout: 5000
+        timeout: 5000,
+        headers: tokenHeader
     })
-
     return instance(config)
 }
